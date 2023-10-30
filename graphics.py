b77c9e7ffef642b29bae55392b911f90c61a672e
Alexander <leonovaleksandrwork@gmail.com>
2023-10-30 14:52:26 +0300

    Added graphics for losses and accuracy

---
+++ graphics.py
@@ -0,0 +1,24 @@
+from matplotlib import pyplot as plt
+
+
+plt.figure(figsize=(50, 50))
+
+def loss_graphic_creation(epochs, loss_value, val_loss_value):
+    epochs = range(1, epochs + 1)
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss_value, "bo", label="Train loss value")
+    plt.plot(epochs, val_loss_value, "b", label="Val loss value")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.legend()
+
+
+def accuracy_graphic_creation(epochs, accuracy, val_accuracy):
+    epochs = range(1, epochs + 1)
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, accuracy, "bo", label="Train accuracy value")
+    plt.plot(epochs, val_accuracy, "b", label="Val accuracy value")
+    plt.xlabel("Epochs")
+    plt.ylabel("Accuracy")
+    plt.legend()
+    plt.show()
--- train.py
+++ train.py
@@ -1,8 +1,11 @@
 from torch import nn
 from torch.utils.data import DataLoader
+
+import graphics
 from model import NeuralNetwork
 from data_loader import *
 from utils import *
+from graphics import *
 
 batch_size = 64
 
@@ -22,9 +25,11 @@ loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
 
-def train(dataloader, model, loss_fn, optimizer):
+def train(dataloader, model, loss_fn, optimizer, loss_values, accuracy_value):
     size = len(dataloader.dataset)
     model.train()
+    train_loss = 0
+    correct = 0
     for batch, (X, y) in enumerate(dataloader):
         X, y = X.to(device), y.to(device)
 
@@ -37,12 +42,19 @@ def train(dataloader, model, loss_fn, optimizer):
         optimizer.step()
         optimizer.zero_grad()
 
+        loss, current = loss.item(), (batch + 1) * len(X)
+        train_loss += loss
+        correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
         if batch % 100 == 0:
-            loss, current = loss.item(), (batch + 1) * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
+    correct = correct / size * 100
+    accuracy_value.append(correct)
+    loss_values.append(train_loss / len(dataloader))
 
-def test(dataloader, model, loss_fn):
+
+def test(dataloader, model, loss_fn, val_loss_values, val_accuracy_value):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     model.eval()
@@ -54,16 +66,26 @@ def test(dataloader, model, loss_fn):
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= num_batches
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    correct = correct / size * 100
+    print(f"Test Error: \n Accuracy: {(correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    val_accuracy_value.append(correct)
+    val_loss_values.append(test_loss)
 
 
-epochs = 5
-for t in range(epochs):
-    print(f"Epoch {t + 1}\n-------------------------------")
-    train(train_dataloader, model, loss_fn, optimizer)
-    test(test_dataloader, model, loss_fn)
-print("Done!")
+if __name__ == '__main__':
+    epochs = 100
+    loss_values = []
+    val_loss_values = []
+    accuracy_value = []
+    val_accuracy_value = []
+    for t in range(epochs):
+        print(f"Epoch {t + 1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer, loss_values, accuracy_value)
+        test(test_dataloader, model, loss_fn, val_loss_values, val_accuracy_value)
+    print("Done!")
 
-torch.save(model.state_dict(), "model.pth")
-print("Saved PyTorch Model State to model.pth")
+    graphics.loss_graphic_creation(epochs, loss_values, val_loss_values)
+    graphics.accuracy_graphic_creation(epochs, accuracy_value, val_accuracy_value)
+
+    torch.save(model.state_dict(), "model.pth")
+    print("Saved PyTorch Model State to model.pth")
